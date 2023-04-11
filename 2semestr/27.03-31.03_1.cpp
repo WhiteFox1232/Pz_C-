7f518df7c05a4558ba9e507c6dd47f11a37e86aa
WhiteFox1232 <91716216+WhiteFox1232@users.noreply.github.com>
2023-04-11 22:28:15 +0300

    Add files via upload

--- 2semestr/27.03-31.03_1.cpp
+++ 2semestr/27.03-31.03_1.cpp
@@ -3,42 +3,42 @@
 #include <unordered_map>
 
 int main() {
-    std::string input_values;
-    std::getline(std::cin, input_values);
-    std::string input_template;
-    std::getline(std::cin, input_template);
+    string input_values;
+    getline(cin, input_values);
+    string input_template;
+    getline(cin, input_template);
 
     //пари "ім'я=значення" з першого рядка
-    std::unordered_map<std::string, std::string> value_map;
-    std::string delimiter = ",";
+    unordered_map<string, string> value_map;
+    string delimiter = ",";
     size_t pos = 0;
-    while ((pos = input_values.find(delimiter)) != std::string::npos) {
-        std::string pair = input_values.substr(0, pos);
+    while ((pos = input_values.find(delimiter)) != string::npos) {
+        string pair = input_values.substr(0, pos);
         size_t equal_pos = pair.find('=');
-        if (equal_pos != std::string::npos) {
-            std::string name = pair.substr(0, equal_pos);
-            std::string value = pair.substr(equal_pos + 1);
+        if (equal_pos != string::npos) {
+            string name = pair.substr(0, equal_pos);
+            string value = pair.substr(equal_pos + 1);
             value_map[name] = value;
         }
         input_values.erase(0, pos + delimiter.length());
     }
     // Остання пара
     size_t equal_pos = input_values.find('=');
-    if (equal_pos != std::string::npos) {
-        std::string name = input_values.substr(0, equal_pos);
-        std::string value = input_values.substr(equal_pos + 1);
+    if (equal_pos != string::npos) {
+        string name = input_values.substr(0, equal_pos);
+        string value = input_values.substr(equal_pos + 1);
         value_map[name] = value;
     }
 
     // Замінити плейсхолдери у другому рядку значеннями
-    std::string placeholder_start = "[";
-    std::string placeholder_end = "]";
-    std::string result = input_template;
+    string placeholder_start = "[";
+    string placeholder_end = "]";
+    string result = input_template;
     pos = 0;
-    while ((pos = result.find(placeholder_start, pos)) != std::string::npos) {
+    while ((pos = result.find(placeholder_start, pos)) != string::npos) {
         size_t end_pos = result.find(placeholder_end, pos + placeholder_start.length());
-        if (end_pos != std::string::npos) {
-            std::string placeholder = result.substr(pos + placeholder_start.length(), end_pos - pos - placeholder_start.length());
+        if (end_pos != string::npos) {
+            string placeholder = result.substr(pos + placeholder_start.length(), end_pos - pos - placeholder_start.length());
             if (value_map.find(placeholder) != value_map.end()) {
                 result.replace(pos, end_pos - pos + placeholder_end.length(), value_map[placeholder]);
             }
@@ -51,7 +51,7 @@ int main() {
         }
     }
 
-    std::cout << result << std::endl;
+    cout << result << endl;
 
     return 0;
 }
